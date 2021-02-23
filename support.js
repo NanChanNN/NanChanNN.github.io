32f3bc9d515080ee856eb25d4e87217987b25422
NanChanNN <72147259+NanChanNN@users.noreply.github.com>
2021-02-23 23:14:02 +0800

    Add files via upload

---
+++ support.js
@@ -0,0 +1,5 @@
+
+var key_list = ["Corporate Income Tax", "Personal Income Tax", "Goods and Services Tax",
+        "Net Investment Returns", "Motor Vehicle Taxes and Fees", "Customs, Excise and Carbon Taxes",
+        "Assets Taxes", "Betting Taxes", "Stamp Duty", "Other Fees and Charges", "Statutory Boards' Contributions",
+        "Others"]
